b1972708c08c6af66ce14f5bab8c20b2728d24c6
ademcatamak <ademcatamak@gmail.com>
2024-08-21 00:27:33 +0200

    index html created for notes functionality

--- notes/Controller.go
+++ notes/Controller.go
@@ -3,9 +3,21 @@ package notes
 import (
 	"PassingNotes/notes/endpoints"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
-func Register(group *gin.RouterGroup) {
+func Register(g *gin.Engine) {
+	g.LoadHTMLFiles("./notes/htmlTemplates/index.html")
+
+	g.GET("", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+	g.GET(":noteId", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	group := g.Group("/api/notes")
+
 	group.POST("", endpoints.CreateNote)
 	group.DELETE("/:id", endpoints.DeleteNote)
 }
